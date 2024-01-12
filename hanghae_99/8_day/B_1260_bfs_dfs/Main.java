5cdcee9ff6ca420941a4de25dccc4ad417f724ca
wondev2321 <joowon2321@gmail.com>
2024-01-13 06:30:10 +0900

    init

--- hanghae_99/8_day/B_1260_bfs_dfs/Main.java
+++ hanghae_99/8_day/B_1260_bfs_dfs/Main.java
@@ -1,20 +1,59 @@
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 
 public class Main{
-    static StringBuilder sb = new StringBuilder();
-    
-    
-    static boolean[] visited;
     static int[][] graph;
+    static boolean[] visited;
 
-    static int nodeC, vertexC, start;
+    private static void dfs(int idx){
+        
 
-    static Queue<Integer> q = new LinkedList<>();
+    }
+
+    private static void bfs(int idx){
+
+    }
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine())
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+
+        
+
+        for(int i = 0; i < N + 1; i++){
+            visited[i] = false; 
+            
+            for(int j = 0; j <N + 1; j++){
+                graph[i][j] = 0;
+            }
+        }
+        
+        for(int j = 0; j < M; j++){
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a + " : " + b);
+            graph[a][b] = 1;
+            graph[b][a] = 1;
+        }
+
+        dfs(V);
+        System.out.println();
+
+        for(int i = 0; i < N + 1; i++){
+            visited[i] = false; 
+        }
+
+    }
 
 }
---
+++ hanghae_99/9_day/B_1068_tree/Main.java
@@ -0,0 +1,5 @@
+package hanghae_99.9_day.B_1068_tree;
+
+public class Main {
+    
+}
